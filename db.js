8068f54515a3a6d8358599bae482035483a1502f
Bilal Kocak <kocakbilal04@gmail.com>
2020-03-21 02:44:46 +0300

    Implemented db

---
+++ db.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,{useNewUrlParser:true,useUnifiedTopology:true});
+
+require('./Models/User');
