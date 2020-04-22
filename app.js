b4f5d1c3545358fcf4180396aed7b48bf5c369df
Bilal Kocak <kocakbilal04@gmail.com>
2020-04-22 23:21:23 +0300

    Added cors

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('./db');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var groupsRouter = require('./routes/groups');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 
